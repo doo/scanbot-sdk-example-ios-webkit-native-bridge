d2a8d96e26efa4a25c8fd1923be703270cf7b86d
Eduard Frank <eddy@scanbot.io>
2019-10-09 10:15:13 +0200

    initial commit

---
+++ ScanbotSdkWebKitNativeBridgeExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanbotSdkWebKitNativeBridgeExample
+//
+//  Created by Eddy on 09.10.19.
+//  Copyright © 2019 doo GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
